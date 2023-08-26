b25d59598ae1695dd0d9acf6d6df2e5676aa6376
김준수 <rhdeotod1023@gmail.com>
2023-08-26 15:44:59 +0900

    짜잘한 오류 수정 / 체크박스 클릭 시 발생하는 이벤트 부여

--- main/main.js
+++ main/main.js
@@ -1,14 +1,17 @@
 let read_Todo_Title = [];
 let read_Todo_Content = [];
+let read_Check_List = [];
 let send_Todo_Title = [];
 let send_Todo_Content = [];
+let send_Check_List = [];
 let titleArrayToJson;
 let contentArrayToJson;
+var isChecked = [];
 
 // 브라우저 준비완료시 작동하는 코드
 window.onload = function() { 
 
-    if (localStorage.getItem('todo_title') == null || localStorage.getItem('todo_content') == null) {
+    if (localStorage.getItem('todo_title') == null || localStorage.getItem('todo_content') == null || JSON.parse(localStorage.getItem('todo_title')).length === 0 || JSON.parse(localStorage.getItem('todo_content')).length === 0) {
         var emptyTodo = `<p style="text-align: center; color: white; font-size: 20px">할 일 목록을 작성해주세요.</p>`
         $('#list_Area').append(emptyTodo);
         $('#all_Complete').css('display', 'none');
@@ -19,7 +22,7 @@ window.onload = function() {
 
         for (i = 0 ; i < read_Todo_Title.length ; i++) {
             var newToDo = `<div class="list">
-            <input type="checkbox" id="check_box" name="check_box"/>
+            <input type="checkbox" class="check_box" name="check_box"/>
             <span class="list_title">${read_Todo_Title[i]}</span>
             <button class="delete"><i class="fa-regular fa-trash-can"></i></button>
             </div>`
@@ -28,8 +31,12 @@ window.onload = function() {
             send_Todo_Content.push(read_Todo_Content[i]);
         }
         $('#all_Complete').css('display', 'inline');
+        for (i = 0; i < $('.list_title').length; i++) { 
+            isChecked[i] = false;
+            
+        }
     } 
-     
+    
 }
 
 // 뒤로가기 버튼 클릭
@@ -40,20 +47,19 @@ $('footer').click(function(e){
         $('#content_Area').html('');
         $('#title').css('padding-left', '345px');
         $('#add').css('display', 'block');
+        $('footer').html('');
+        $('footer').append(`<button id="all_Complete" style="text-align: center;">모두 완료</button>`);
         for (i = 0; i < read_Todo_Title.length; i++) {
             var newToDo = `<div class="list">
-                <input type="checkbox" id="check_box" name="check_box"/>
+                <input type="checkbox" class="check_box" name="check_box"/>
                 <span class="list_title">${read_Todo_Title[i]}</span>
                 <button class="delete"><i class="fa-regular fa-trash-can"></i></button>
                 </div>`
             $('#list_Area').append(newToDo);
-            send_Todo_Title.push(read_Todo_Title[i]);
-            send_Todo_Content.push(read_Todo_Content[i]);
+            // send_Todo_Title.push(read_Todo_Title[i]);
+            // send_Todo_Content.push(read_Todo_Content[i]);
         }
-
-
     }
-    
 })
 
 // 할 일 목록 추가 버튼
@@ -63,14 +69,14 @@ $('#add').click(function(){
 })
 
 $('#add_Btn').click(function(){
-    if (localStorage.getItem('todo_title') == null) {
+    if (localStorage.getItem('todo_title') == null || JSON.parse(localStorage.getItem('todo_title')).length === 0) {
         $('#list_Area').html('');
     }
 
     $('.add_Area').css('display', 'none');
 
     var newToDo = `<div class="list">
-    <input type="checkbox" id="check_box" name="check_box"/>
+    <input type="checkbox" class="check_box" name="check_box"/>
     <span class="list_title">${$('#ToDo_title').val()}</span>
     <button class="delete"><i class="fa-regular fa-trash-can"></i></button>
     </div>`
@@ -85,6 +91,7 @@ $('#add_Btn').click(function(){
     localStorage.setItem('todo_title', titleArrayToJson);
     localStorage.setItem('todo_content', contentArrayToJson);
     
+    location.reload()
     
 })
 
@@ -92,10 +99,20 @@ $('#close_Btn').click(function(){
     $('.add_Area').css('display', 'none');
 });
 
-// title 클릭 시 화면 html 변경
+
+
+
+
+
+
 $('#list_Area').click(function(e){
 
+    // console.log(isChecked);
+    
+
+    // title 클릭 시 화면 html 변경
     var indexTodo = read_Todo_Title.indexOf(`${e.target.textContent}`);
+    
     
     for (i = 0; i < $('.list_title').length; i++) {
         if (e.target == document.getElementsByClassName('list_title')[i]) {
@@ -107,25 +124,68 @@ $('#list_Area').click(function(e){
             $('#title').html(`${e.target.textContent}`)
             $('#content_Area').html(`${read_Todo_Content[indexTodo]}`)
         }
-    }
-    
-});
+    } 
 
-// 체크박스 + 삭제
-
-$('#list_Area').click(function(e){
-
-    if (e.target == document.getElementById('check_box')) {
-        e.target.nextElementSibling.style.textDecoration = "line-through";
-    }
-
+    // 체크박스 
     for (i = 0; i < $('.list_title').length; i++) { 
-        if (e.target == document.getElementsByClassName('fa-regular fa-trash-can')[i]) {
-            $(e.target.parentNode.parentNode).html('')
-            // TODO: 프론트에서만 삭제된것이 아니라 백에서도 삭제되도록
+        if (e.target == document.getElementsByClassName('check_box')[i]) {
+
+            if (isChecked[i] == false) {
+                e.target.nextElementSibling.style.textDecoration = "line-through";
+                $('.check_box').eq(i).prop('checked',true);
+                isChecked[i] = true;
+            } else {
+                e.target.nextElementSibling.style.textDecoration = "none";
+                $('.check_box').eq(i).prop('checked',false);
+                isChecked[i] = false;
+            }
+
+            
         }
     }
 
+
+    var changedTitle = send_Todo_Title;
+    var changedContent = send_Todo_Content;
     
-    
+    // 삭제
+    for (i = 0; i < $('.list_title').length; i++) { 
+        if (e.target == document.getElementsByClassName('fa-regular fa-trash-can')[i]) {
+
+            if (send_Todo_Title.length < 2){
+                var emptyTodo = `<p style="text-align: center; color: white; font-size: 20px">할 일 목록을 작성해주세요.</p>`
+                $('#list_Area').append(emptyTodo);
+                $('#all_Complete').css('display', 'none');
+            } 
+
+            let seletedTitle = e.target.parentElement.previousSibling.previousSibling.outerText;
+            let indexTodo = changedTitle.indexOf(`${seletedTitle}`);
+
+            changedTitle.splice(indexTodo, 1);
+            changedContent.splice(indexTodo, 1);
+
+            titleArrayToJson = JSON.stringify(changedTitle);
+            contentArrayToJson = JSON.stringify(changedContent);
+            
+            localStorage.setItem('todo_title', titleArrayToJson);
+            localStorage.setItem('todo_content', contentArrayToJson);
+
+            $(e.target.parentNode.parentNode).remove();
+                
+                            
+        }
+    }
 });
+
+// 모두 완료 버튼
+
+$('#all_Complete').click(function(){
+    for (i = 0; i < $('.list_title').length; i++) { 
+        $('.list_title').eq(i).css('textDecoration', 'line-through');
+        $('.check_box').prop('checked',true)
+        isChecked[i] = true;
+    }
+
+    //TODO: 체크여부 배열을 local storage에도 저장해서 새로고침해도 체크 된 것 표기하기
+})
+
